b3f637835dd905022df049a663bb4e3ebc8f9468
Aarit Jayaswal <aaritrj@gmail.com>
2025-06-30 16:59:39 -0700

    fin

--- src/_04_Light_Switches/LightSwitches.java
+++ src/_04_Light_Switches/LightSwitches.java
@@ -48,14 +48,18 @@ public class LightSwitches implements GameControlScene {
             };
 
     // 8-bit bitmap. Leave as int so methods won't have to cast to a byte
-    int lightsOnOff = 0;
+    byte lightsOnOff = 0;
 
     /*
      * This method should check if the specified light is on, example:
      * index = 6        // return true if pink is on (bit 6 == 1)
      */
     boolean isLightOn(int index) {
-        return false;
+    	if((gbi(index)&lightsOnOff)==0) {
+        return true;
+    	}else {
+    		return false;
+    	}
     }
     
     /*
@@ -63,7 +67,7 @@ public class LightSwitches implements GameControlScene {
      * index = 4        // turn off yellow only (set bit 4 = 1)
      */
     void turnLightOn(int index) {
-        
+        lightsOnOff=(byte) (lightsOnOff|gbi(index));
     }
     
     /*
@@ -71,7 +75,7 @@ public class LightSwitches implements GameControlScene {
      * index = 0        // turn off blue only (set bit 0 = 0)
      */
     void turnLightOff(int index) {
-        
+          lightsOnOff = (byte) (lightsOnOff&(~gbi(index)));
     }
     
     /*
@@ -79,7 +83,7 @@ public class LightSwitches implements GameControlScene {
      * lightsBitmap = 0b01100110  // lights 1, 2, 5, 6 on
      */
     void turnMultiLightsOn(int lightsBitmap) {
-        
+        lightsOnOff=(byte) (lightsOnOff|lightsBitmap);
     }
     
     /*
@@ -87,7 +91,7 @@ public class LightSwitches implements GameControlScene {
      * lightsBitmap = 0b10000001  // lights 0, 7 off
      */
     void turnMultiLightsOff(int lightsBitmap) {
-        
+    	lightsOnOff=(byte) (lightsOnOff|(~lightsBitmap));
     }
     
     /*
@@ -100,7 +104,7 @@ public class LightSwitches implements GameControlScene {
      *                               orange(3) and yellow(4) on
      */
     void toggleLights(int lightsBitmap) {
-        
+    	lightsOnOff=(byte) (lightsOnOff^lightsBitmap);
     }
     
     void runLightSequence1() {
@@ -230,5 +234,14 @@ public class LightSwitches implements GameControlScene {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+    public byte gbi(int index) {
+    	String binStr = new StringBuilder("00000000").replace(8-index, 8-index, "1").toString();
+    			byte out=0;
+    	for (int i = binStr.length()-1; i > -1; i--) {
+    		out+=Character.getNumericValue(binStr.charAt(i))*(Math.pow(2, binStr.length()-1-i));
+			System.out.println(binStr.substring(i,i+1)+" =" + Integer.parseInt(binStr.substring(i,i+1))*(Math.pow(2, binStr.length()-1-i)));
+    	}
+    	return out;
     }
 }
